e313aebba561a5cce20ca0cd8eb14a4bc8f47eee
Alban Peignier <alban@tryphon.eu>
2012-10-15 17:58:34 +0200

    Fixes syntax ambiguity

--- spec/steto/beep_reporter_spec.rb
+++ spec/steto/beep_reporter_spec.rb
@@ -4,17 +4,17 @@ describe Steto::BeepReporter do
 
   it "should beep twice when one of checks is critical" do
     subject.should_receive(:beep).with(2)
-    subject.report [ mock :status => Steto::Status.new(:critical) ]
+    subject.report [ mock(:status => Steto::Status.new(:critical)) ]
   end
 
   it "should beep when one of checks is warning" do
     subject.should_receive(:beep)
-    subject.report [ mock :status => Steto::Status.new(:warning) ]
+    subject.report [ mock(:status => Steto::Status.new(:warning)) ]
   end
 
   it "should not beep when no check is warning or critical" do
     subject.should_not_receive(:beep)
-    subject.report [ mock :status => Steto::Status.new(:info) ]
+    subject.report [ mock(:status => Steto::Status.new(:info)) ]
   end
 
 end
